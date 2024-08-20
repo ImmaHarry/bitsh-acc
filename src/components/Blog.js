32b21d5fb166c145bea7dd9f1126bffbad8c4ee3
harrydadev <harrydadev@gmail.com>
2024-08-20 23:54:58 +0530

    feat: Add lazy loading for images and update Blog component
    The code changes introduce lazy loading for images, improving the page load performance. Additionally, the Blog component has been updated to include the BlogContent, BlogImage, and BlogWriteup components, enhancing the structure and content of the blog section.

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import BlogTitle from './BlogTitle'
-import Metadata from './Metadata'
+import BlogContent from './BlogContent'
+
 
 const Blog = () => {
   return (
-    <section className='mx-16 md:mx-20 lg:mx-28 xl:mx-32 flex flex-col justify-center bg-zinc-800'>
-        <BlogTitle/>
+    <section className='mx-16 md:mx-20 lg:mx-28 xl:mx-32 flex flex-col justify-center bg-zinc-800 '>
+      <BlogContent/>
     </section>
   )
 }
--- src/components/BlogContent.js
+++ src/components/BlogContent.js
@@ -1,18 +1,22 @@
 import React from 'react'
 import Metadata from './Metadata'
+import BlogImage from './BlogImage'
+import BlogWriteup from './BlogWriteup'
 
-const BlogTitle = () => {
+const BlogContent = () => {
   return (
     <div id='blog-heading' className='max-w-screen-md mx-auto'>
         <div className="flex items-center space-x-2 md:space-x-4 mt-4 text-left">
-            <div className="h-16 md:h-20 w-1 md:w-2 bg-white"></div>
-            <h1 className="text-xl md:text-3xl font-medium text-white font-roboto_slab">
+            <div className="h-12 md:h-20 w-1 md:w-2 bg-white"></div>
+            <h1 className="text-lg sm:text-xl md:text-3xl font-medium text-white font-roboto_slab">
                 Inductions for the Academic Counselling Cell Are Now Officially Underway
             </h1>
         </div>
         <Metadata/>
+        <BlogImage/>
+        <BlogWriteup/>
     </div>
   )
 }
 
-export default BlogTitle
+export default BlogContent
