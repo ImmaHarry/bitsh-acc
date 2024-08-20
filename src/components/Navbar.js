e484adf70a3d48f913de1010c184b7d8810e6834
harrydadev <harrydadev@gmail.com>
2024-08-21 01:15:14 +0530

    ```text feat: Update Navbar component with id attribute

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,9 @@
 import React from 'react';
 
 const Navbar = () => {
+
   return (
-    <nav className="flex items-center justify-between mx-16 md:mx-20 lg:mx-28 xl:mx-32 py-4">
+    <nav id="navbar" className="flex items-center justify-between mx-16 md:mx-20 lg:mx-28 xl:mx-32 py-4">
       <div className="text-2xl font-bold text-yellow-400 font-roboto_slab flex items-center cursor-pointer">
         <svg xmlns="http://www.w3.org/2000/svg" className='mr-[0.875rem]' width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-send"><path d="m22 2-7 20-4-9-9-4Z"/><path d="M22 2 11 13"/></svg>
         ACC Blogs
